5420c61ec2028f75950e768fb9b6c0a0c17f2cbc
yinquanchao <yinquanchao@cscec.com>
2022-02-10 10:23:08 +0800

    1-rectangle

---
+++ src/1-rectangle/draw.js
@@ -0,0 +1,6 @@
+function main(params) {
+	const canvas = document.getElementById('rect');
+	const ctx = canvas.getContext('2d');
+	ctx.fillStyle = 'rgba(0, 0, 255, 1.0)';
+	ctx.fillRect(120, 10, 150, 150)
+}
